0962d5c7c9553c0fc1ffa29c3311d2e21d93d395
Mindy <mindy@valhallaonlineservices.com>
2025-08-20 22:11:25 +0800

    Change scripts version

--- inc/scripts.php
+++ inc/scripts.php
@@ -7,7 +7,7 @@ function bellaworks_scripts() {
 		'bellaworks-style',
 		 get_stylesheet_uri(),
 		 array(),
-		 '2.01'
+		 '2.1'
 	);
   wp_enqueue_style( 'swiper-style', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css' );
   wp_enqueue_style( 'jquery-ui-style', 'https://code.jquery.com/ui/1.13.3/themes/smoothness/jquery-ui.css' );
@@ -26,7 +26,6 @@ function bellaworks_scripts() {
     array(), '1.13.3', 
     false 
   );
-
 
 	wp_enqueue_script( 
 			'gsap',
@@ -79,7 +78,7 @@ function bellaworks_scripts() {
 	wp_enqueue_script( 
 		'bellaworks-custom', 
 		get_template_directory_uri() . '/assets/js/custom.js', 
-		array(), '2.4', 
+		array(), '2.5', 
 		true 
 	);
 
